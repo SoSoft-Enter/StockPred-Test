aed708fa5b1e7661e2f4b3e2d774496cbd1d185a
CANDRA BAGUS AINUR R <abaguscandra@gmail.com>
2024-11-13 14:15:55 +0700

    Add new activity and directory for activity

---
+++ app/src/main/java/com/dicoding/stockpred/Input/PredictInput.kt
@@ -0,0 +1,21 @@
+package com.dicoding.stockpred.Input
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.stockpred.R
+
+class PredictInput : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_predict_input)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
